4737be661eadd572f63a4adc83707fe882ec3410
Andrew Sanetra <asanetras@gmail.com>
2017-10-30 15:50:16 -0500

    write c program for in and out

---
+++ anathemabot/fake_bot.c
@@ -0,0 +1,15 @@
+#include "fake_bot.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char * argv[]) {
+
+    // open named pipe
+
+    // stdin to this program goes to input fd of named pipe
+
+    // output fd of named pipe goes to stdout of this program
+
+}
---
+++ anathemabot/fake_bot.h
@@ -0,0 +1,6 @@
+#ifndef ANATHEMABOT_FAKE_BOT_H
+#define ANATHEMABOT_FAKE_BOT_H
+
+#define NAMED_PIPE_PREFIX "pipes/anathema_bot_"
+
+#endif //ANATHEMABOT_FAKE_BOT_H
