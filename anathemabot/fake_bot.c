950a5e9f0b78feda549023aa8d09edee9f4238af
Andrew Sanetra <asanetras@gmail.com>
2017-10-30 19:58:58 -0500

    Add missing header to fake_bot.c

--- anathemabot/fake_bot.c
+++ anathemabot/fake_bot.c
@@ -1,11 +1,12 @@
 #include "fake_bot.h"
 
 #include <assert.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
 
